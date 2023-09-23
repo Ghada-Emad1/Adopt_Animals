81d47cbec8a4ef670ab35bb3174d97bb9effd1e2
ghada <ghadaemad011@gmail.com>
2023-09-24 00:33:06 +0300

    make carousel class component

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,26 @@
+import { Component } from "react";
+
+export class Carousel extends Component {
+  state = {
+    active: 0,
+  };
+
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+  render(){
+    const {active}=this.state;
+    const {images}=this.props;
+    return(
+        <div className="carousel">
+            <img src={images[active]} alt="animal"/>
+            <div className="carousel-smaller">
+                {images.map((photo,index)=>(
+                    console.log(photo)
+                ))}
+            </div>
+        </div>
+    )
+  }
+}
+
