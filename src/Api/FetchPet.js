ca490cb1dbd287c9f3c3fa0fd2b5863cf0445421
ghada <ghadaemad011@gmail.com>
2023-10-02 23:20:52 +0300

    edit url path

--- src/Api/FetchPet.js
+++ src/Api/FetchPet.js
@@ -1,7 +1,7 @@
 export const  FetchPet=async({queryKey})=>{
     const id = queryKey[1];
     //console.log(queryKey)
-  const apiRes = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
+  const apiRes = await fetch(`https://pets-v2.dev-apis.com/pets?id=${id}`);
  //console.log(apiRes)
   if (!apiRes.ok) {
     throw new Error(`details/${id} fetch not ok`);
