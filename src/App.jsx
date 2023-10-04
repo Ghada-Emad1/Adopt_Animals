a8e50b4eead6cb76c923b87d5c413fc6066ec6be
ghada <ghadaemad011@gmail.com>
2023-10-04 12:35:20 +0300

    add context provider to App so i can access context to Details and SearchParams Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-
 import { SearchParam } from "./components/SearchParam";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ErrorBoundary } from "./Errors/ErrorBoundary";
+import { AdoptAnimalPetContext } from "./contexts/AdoptAnimalPetContext";
 import Details from "./components/Details";
+import { useState } from "react";
 function App() {
+  const AdoptPet = useState(null);
+  //console.log(AdoptPet)
   const queryclient = new QueryClient({
     defaultOptions: {
       queries: {
@@ -15,17 +17,19 @@ function App() {
   });
   return (
     <BrowserRouter>
-      <QueryClientProvider client={queryclient}>
-        <div>
-          <header>
-            <Link to="/">Adopt Me!ee</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details/>} />
-            <Route path="/" element={<SearchParam />} />
-          </Routes>
-        </div>
-      </QueryClientProvider>
+      <AdoptAnimalPetContext.Provider value={AdoptPet}>
+        <QueryClientProvider client={queryclient}>
+          <div>
+            <header>
+              <Link to="/">Adopt Me!ee</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParam />} />
+            </Routes>
+          </div>
+        </QueryClientProvider>
+      </AdoptAnimalPetContext.Provider>
     </BrowserRouter>
   );
 }
